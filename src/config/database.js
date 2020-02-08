33185aeb192b2840d311c788aa1aa89dfa8c4e1f
Evandro Gibicoski <evandrogibicoski@outlook.com>
2020-02-08 18:41:46 -0300

    Update porject

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'sql_node',
+    password: 'docker',
+    database: {
+        timestamps: true,
+        underscored: true,
+    },
+};
