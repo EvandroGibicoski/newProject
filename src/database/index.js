eb77fc0653a73f4d414f609ea1ea4291f4bdf20a
Evandro Gibicoski <evandrogibicoski@outlook.com>
2020-02-20 20:57:01 -0300

    update database

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
 const Sequelize = require('sequelize');
-
 const dbConfig = require('../config/database');
 
-const User = require('../model/User');
+const User = require('../models/User');
+const Address = require('../models/Addresses');
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
+Address.init(connection);
 
 module.exports = connection;
